9a76b857d453c031779c71b6223d37df5f746d4a
Sabit <ceferovsabit999@gmail.com>
2024-11-01 22:42:11 +0400

    added validepassword and custom

---
+++ task-custom/task-custom/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Channels;
+
+namespace task_custom
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                string customTrim = Console.ReadLine(); 
+                string trimmedResult = CustomTrim(customTrim); 
+                Console.WriteLine(trimmedResult);
+            }
+
+            static string CustomTrim(string trim)
+            {
+                int start = 0; 
+                int end = trim.Length - 1; 
+
+                
+                while (start < trim.Length && char.IsWhiteSpace(trim[start]))
+                {
+                    start++;
+                }
+
+                
+                while (end >= start && char.IsWhiteSpace(trim[end]))
+                {
+                    end--;
+                }
+
+                
+                if (start > end)
+                {
+                    return "";
+                }
+
+                string result = ""; 
+
+               
+                for (int i = start; i <= end; i++)
+                {
+                    result += trim[i];
+                }
+
+                return result;
+            }
+        }
+ }
+
---
+++ task-validatepassword/task-validatepassword/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+
+namespace task_validatepassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your password: ");
+            string password = Console.ReadLine();
+            bool validPassword = ValidatePassword(password);
+            Console.WriteLine(validPassword);
+        }
+        static bool ValidatePassword(string password)
+        {
+            bool isDigit = false;
+            bool isSpcChar = false;
+
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            if (!char.IsUpper(password[0]))
+            {
+                return false;
+            }
+            foreach (char c in password)
+            {
+                if (char.IsDigit(c))
+                {
+                    isDigit=true;
+                }
+                if(!char.IsLetterOrDigit(c))
+                {
+                    isSpcChar = true;
+                    break;
+                }
+            }
+            return isDigit && isSpcChar;
+
+        }
+    }
+}
